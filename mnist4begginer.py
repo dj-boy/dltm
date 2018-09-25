44ed6961c715c0c0c00ebced6729dcdcd4071a87
Allen-Qiu <jiangtaoqiu@gmail.com>
2018-09-25 22:58:38 +0800

    Add files via upload

--- mnist4begginer.py
+++ mnist4begginer.py
@@ -19,7 +19,7 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
